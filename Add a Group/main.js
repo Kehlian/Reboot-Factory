ccaa3bd403471d28817d3ddbfcf35ff90a79a8fb
Kehlian <clement.schmouker@gmail.com>
2015-01-03 09:52:48 +0100

    Added the right files

---
+++ Add a Group/main.js
@@ -0,0 +1,12 @@
+$(document).ready(main);
+
+var cButton = $(".button");
+var cGroup = $(".groupA");
+
+function main()
+{
+    cButton.click(function()
+    {
+        console.log("test");
+    })
+}
