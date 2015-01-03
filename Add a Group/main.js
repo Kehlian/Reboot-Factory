dc4ee43e7491c2de6612ed4755f518b08fb0e6cb
Kehlian <clement.schmouker@gmail.com>
2015-01-03 10:02:57 +0100

    Cool animation of group added
    Slides smoothly :D

--- Add a Group/main.js
+++ Add a Group/main.js
@@ -2,11 +2,30 @@ $(document).ready(main);
 
 var cButton = $(".button");
 var cGroup = $(".groupA");
+var cGroup2 = $(".groupB");
+var iGroupIndex = 0;
 
 function main()
 {
     cButton.click(function()
     {
-        console.log("test");
+        if (iGroupIndex == 0) {
+            cGroup.animate(
+                { marginLeft: "0" },
+                300
+                )
+            iGroupIndex += 1;
+        }
+        else if (iGroupIndex == 1) {
+            cGroup2.animate(
+                { marginLeft:"0" },
+                300
+                )
+            iGroupIndex += 1;
+            cGroup.animate(
+                { marginTop:"15%" },
+                300
+                )
+        }
     })
 }
